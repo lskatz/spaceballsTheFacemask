25d07e2c0ad3586aca9b98f18a73195215d25163
Lee Katz <lskatz@gmail.com>
2021-01-06 20:31:25 -0500

    added some more structure

--- spaceballsTheFacemask.py
+++ spaceballsTheFacemask.py
@@ -2,6 +2,13 @@
 
 from advent import *
 
+spec = {}
+
+def showHelp():
+  print("""
+  This would show some help and maybe even draw from the JSON for the spec
+  """)
+
 game = Game()
 
 lobby = game.new_location(
@@ -9,16 +16,34 @@ lobby = game.new_location(
 There is a desk with a spreadsheet
 """)
 
-spec = lobby.new_object("spreadsheet",
+spreadsheet = lobby.new_object("spreadsheet",
   "a copy of the SARS-CoV-2 metadata spec")
 
 def fill(self, actor, noun, words):
-  if (noun and noun != "out") or words:
+  if not noun:
+    print(showHelp())
+    return True
+  if (noun and noun != "in") and words:
     return False
-  print("You fill out the form like a true public health hero!")
+  key = words[0]
+  spec[key] = words[1]
+  return True
+lobby.add_verb(Verb(fill,'fill'))
+
+def make(self, actor, noun, words):
+  if (noun and (noun != "spec" or noun != "spreadsheet")) and words:
+    return False
+  tsv = ""
+  for field in spec.keys():
+    tsv += field + "\t"
+  tsv += "\n"
+  for field in spec.keys():
+    tsv += spec[field] + "\t"
+  tsv += "\n"
+  print(tsv)
   return True
 
-lobby.add_verb(Verb(fill,'fill'))
+lobby.add_verb(Verb(make,'make'))
 
 hero = game.new_player(lobby)
 
